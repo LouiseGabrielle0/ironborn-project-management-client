4a839487de1d41b72e80da0cbb1da7d3dbfb31e7
Louise <louisegabriellecc@gmail.com>
2022-05-18 09:49:03 +0100

    Context and NavBar

---
+++ src/context/auth.context.js
@@ -0,0 +1,33 @@
+// src/context/auth.context.js
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+// const API_URL = "http://localhost:5005"; - Luis removes this and puts it in .env 
+
+const AuthContext = React.createContext(); //Another way to import ThemeContext -
+// ^^ This is a context object that gives us a provider and something to wrap around the app
+
+// exposing these -  isLoggedIn, isLoading, user  - which can change over time so saved in a state
+
+function AuthProviderWrapper(props) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  // To enable access to the values exposed through the Provider’s (AuthContext.Provider) value prop, we have to wrap the AuthProviderWrapper around other components. 
+  
+  /* 
+    Functions for handling the authentication status (isLoggedIn, isLoading, user)
+    will be added here later in the next step
+  */
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, isLoading, user }}> 
+      {props.children}
+    </AuthContext.Provider>
+  )
+}
+
+export { AuthProviderWrapper, AuthContext };
+
+// ^^^ exporting the component
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {AuthProviderWrapper} from './context/auth.context'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <AuthProviderWrapper>
       <App />
+      </AuthProviderWrapper>
     </BrowserRouter>
   </React.StrictMode>
 );
@@ -17,4 +20,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
