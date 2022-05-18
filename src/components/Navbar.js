01037bdcd5cbcfec86611a70c3fa330d4e39bfb2
Louise <louisegabriellecc@gmail.com>
2022-05-18 09:49:48 +0100

    Nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,31 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import {AuthContext} from '../context/auth.context'
 
 function Navbar(){
+
+  
+    const {isLoggedIn, isLoading, user} = useContext(AuthContext) //returns an object which can be stored in a variable or can use object destruction
+    
     return (
         <nav>
             <NavLink to="/">Home</NavLink> | 
             <NavLink to="/projects">Projects</NavLink> | 
-            <NavLink to="/projects/create">New Project</NavLink>
+            <NavLink to="/projects/create">New Project</NavLink> | | |
+            {/* {isLoggedIn
+                ? <span> Welcome</span>
+                : <NavLink to="/login">Login</NavLink> <<<<< another option
+            } */} 
+            
+            {isLoggedIn &&
+                <span> Welcome</span>
+                }
+            {!isLoggedIn &&
+            <>
+                <NavLink to="/login">Login</NavLink> |
+                <NavLink to="/singup">Sign Up</NavLink> 
+                </>
+                }
         </nav>
     );
 }
