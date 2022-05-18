9beee3f8c8aafc57b9aeedfa1ea58fe5f76094bb
Louise <louisegabriellecc@gmail.com>
2022-05-18 14:32:56 +0100

    Custom Wrapper - IsPrivate

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import axios from "axios";
-import './App.css';
-import Navbar from './components/Navbar';
-import { Route, Routes } from 'react-router-dom';
-import ProjectListPage from './pages/ProjectListPage';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import ProjectListPage from "./pages/ProjectListPage";
 import { useEffect, useState } from "react";
 import AddProjectPage from "./pages/AddProjectPage";
 import EditProjectPage from "./pages/EditProjectPage";
 import DeleteProjectPage from "./pages/DeleteProjectPage";
 import SignUpPage from "./pages/SignUpPage";
-import LoginPage from "./pages/LoginPage"
+import LoginPage from "./pages/LoginPage";
+import IsPrivate from "./components/isPrivate";
 
 function App() {
-  
   const [projects, setProjects] = useState(null);
 
   const getProject = () => {
-    axios.get(`${process.env.REACT_APP_API_URL}/projects`)
-    .then( response => {
-      setProjects(response.data);
-    })
-    .catch( e => console.log("error getting projects from API...", e))
-  }
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/projects`)
+      .then((response) => {
+        setProjects(response.data);
+      })
+      .catch((e) => console.log("error getting projects from API...", e));
+  };
 
   useEffect(() => {
-   getProject();
+    getProject();
   }, []);
 
   return (
@@ -31,14 +32,52 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path='/' element={<h1>Welcome</h1>} />
-        <Route path='/projects' element={<ProjectListPage projects={projects} />} />
-        <Route path='/projects/create' element={<AddProjectPage getprojects={getProject} />} />
-        <Route path='/projects/:projectId/edit' element={<EditProjectPage projects={projects} getprojects={getProject} />} />
-        <Route path='/projects/:projectId/delete' element={<DeleteProjectPage projects={projects} getprojects={getProject} />} />
-        
-        <Route path="/signup" element={<SignUpPage/>}/>
-        <Route path="login" element={<LoginPage/>}/>
+        <Route path="/" element={<h1>Welcome</h1>} />
+        <Route
+          path="/projects"
+          element={
+            <IsPrivate>
+              {" "}
+              <ProjectListPage projects={projects} />{" "}
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/projects/create"
+          element={
+            <IsPrivate>
+              {" "}
+              <AddProjectPage getprojects={getProject} />{" "}
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/projects/:projectId/edit"
+          element={
+            <IsPrivate>
+              {" "}
+              <EditProjectPage
+                projects={projects}
+                getprojects={getProject}
+              />{" "}
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/projects/:projectId/delete"
+          element={
+            <IsPrivate>
+              {" "}
+              <DeleteProjectPage
+                projects={projects}
+                getprojects={getProject}
+              />{" "}
+            </IsPrivate>
+          }
+        />
+
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="login" element={<LoginPage />} />
       </Routes>
     </div>
   );
